150018d4014c1fa45884af3f586ab1de538c62c0
Ivan Lohvynenko <stradayqq@gmail.com>
2026-08-09 19:29:20 +0300

    fix question

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fileinput import close
+
 import dns.resolver
 from ipwhois import IPWhois
 
@@ -36,5 +38,5 @@ def print_whois(ip):
         print(f"    whois for {ip}: Error ({e})")
 
 if __name__ == "__main__":
-    target_domain = "freehost.com.ua"
+    target_domain = input("Enter the domain to check: ")    
     check_dns_records(target_domain)
